1afbd2a3e82202b702afde240f2bdd06c457e6f2
coreybolson <coreybolson@0c9581b5-2034-7111-aa85-6f84924adf1f>
2010-04-29 23:02:28 +0000

    adding a file for easy synthesis, this one only synthesizes ALU

--- pt_scripts/README.txt
+++ pt_scripts/README.txt
@@ -1,7 +1,12 @@
 README file for using primetime with tcl scripts
 
 syntax:
+
+primetime:
 pt_shell -file <filename>.tcl
+
+design compiler: can compile just the ALU or the entire FU
+dc_shell-t -f compile_<block>.tcl
 
 notes:
 do not place the pdk database in the SVN repository
---
+++ pt_scripts/compile_ALU.tcl
@@ -0,0 +1,88 @@
+#/**************************************************/
+#/* Compile Script for Synopsys                    */
+#/*                                                */
+#/* dc_shell-t -f compile_ALU.tcl                  */
+#/*                                                */
+#/* IBM 65nm PDK		                           */
+#/**************************************************/
+
+#/* All verilog files, separated by spaces         */
+# for synthesis of just the ALU
+set my_verilog_files [list Alu.v]
+
+#/* Top-level Module                               */
+set my_toplevel Alu
+
+#/* The name of the clock pin. If no clock-pin     */
+#/* exists, pick anything                          */
+set my_clock_pin clk
+
+#/* Target frequency in MHz for optimization       */
+set my_clk_freq_MHz 700
+
+#/* Delay of input signals (Clock-to-Q, Package etc.)  */
+set my_input_delay_ns 0.1
+
+#/* Reserved time for output signals (Holdtime etc.)   */
+set my_output_delay_ns 0.1
+
+
+#/**************************************************/
+#/* No modifications needed below                  */
+#/**************************************************/
+set IBM_65nm_PDK ./
+set search_path [concat  $path  $IBM_65nm_PDK]
+set alib_library_analysis_path $IBM_65nm_PDK
+
+set link_library [set target_library cp65npksdst_tt1p2v25c.db]
+set target_library "cp65npksdst_tt1p2v25c.db"
+define_design_lib WORK -path ./WORK
+set verilogout_show_unconnected_pins "true"
+#set_ultra_optimization true
+#set_ultra_optimization -force
+
+analyze -f verilog $my_verilog_files
+
+elaborate $my_toplevel
+
+current_design $my_toplevel
+
+link
+uniquify
+
+set my_period [expr 1000 / $my_clk_freq_MHz]
+
+set find_clock [ find port [list $my_clock_pin] ]
+if {  $find_clock != [list] } {
+   set clk_name $my_clock_pin
+   create_clock -period $my_period $clk_name
+} else {
+   set clk_name vclk
+   create_clock -period $my_period -name $clk_name
+}
+
+set_driving_cell  -lib_cell INVX1  [all_inputs]
+set_input_delay $my_input_delay_ns -clock $clk_name [remove_from_collection [all_inputs] $my_clock_pin]
+set_output_delay $my_output_delay_ns -clock $clk_name [all_outputs]
+
+compile -ungroup_all -map_effort medium
+
+compile -incremental_mapping -map_effort medium
+
+check_design
+report_constraint -all_violators
+
+set filename [format "%s%s"  $my_toplevel ".vh"]
+write -f verilog -output $filename
+
+set filename [format "%s%s"  $my_toplevel ".sdc"]
+write_sdc $filename
+
+#set filename [format "%s%s"  $my_toplevel ".db"]
+#write -f db -hier -output $filename
+
+redirect timing.rep { report_timing }
+redirect cell.rep { report_cell }
+redirect power.rep { report_power }
+
+quit
