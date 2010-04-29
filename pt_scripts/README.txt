e63236ecdd98469457ec301590bf3c310d6dcb6b
coreybolson <coreybolson@0c9581b5-2034-7111-aa85-6f84924adf1f>
2010-04-29 21:23:32 +0000

    renaming behavioral verilog files to .v

--- pt_scripts/README.txt
+++ pt_scripts/README.txt
@@ -2,3 +2,6 @@ README file for using primetime with tcl scripts
 
 syntax:
 pt_shell -file <filename>.tcl
+
+notes:
+do not place the pdk database in the SVN repository
--- pt_scripts/functional_unit.v
+++ pt_scripts/functional_unit.v
@@ -5,5 +5,24 @@
  Date: 4/29/2010
 */
 module functional_unit();
+	
+	////////
+	// IO //
+	////////
+	output [31:0] Z;
+	output [3:0] FLAGS;
+	input [31:0] A, B;
+	input [] INST;
+	input CI;
+	
+	// alu
+	Alu alu_inst (.A(),.B(),.DI(),.INST(),.CI(),.FirstCyc(),.Z(),.DO(),.FLAGS());
+		
+	// barrel shifter
+	
 
+		
+	// MADD unit
+	
+		
 endmodule
