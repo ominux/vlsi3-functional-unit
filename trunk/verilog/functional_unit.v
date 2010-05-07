50dfa425a573fc9a5a2512de3f3fd7e367bb7231
felispuma@gmail.com <felispuma@gmail.com@0c9581b5-2034-7111-aa85-6f84924adf1f>
2010-05-07 00:25:53 +0000

    Initial MADD inclusion

--- trunk/verilog/functional_unit.v
+++ trunk/verilog/functional_unit.v
@@ -4,25 +4,40 @@
  Author: Corey Olson
  Date: 4/29/2010
 */
-module functional_unit();
+
+`timescale 1ns / 10 ps
+
+module functional_unit(Z, FLAGS, A, B, C, INST, CI);
 	
 	////////
 	// IO //
 	////////
 	output [31:0] Z;
 	output [3:0] FLAGS;
-	input [31:0] A, B;
-	input [] INST;
+	input [31:0] A, B, C;
+	input [4:0] INST;
 	input CI;
 	
+        wire [31:0] Alu_Z, MADD_Z;
+
 	// alu
-	Alu alu_inst (.A(),.B(),.DI(),.INST(),.CI(),.FirstCyc(),.Z(),.DO(),.FLAGS());
+	Alu alu_inst (.A(A),
+                      .B(B),
+                      .DI(),
+                      .INST(INST[3:0]),
+                      .CI(CI),
+                      .FirstCyc(),
+                      .Z(Alu_Z),
+                      .DO(),
+                      .FLAGS());
 		
 	// barrel shifter
 	
 
-		
 	// MADD unit
-	
-		
+        MADD madd (.A(A), .B(B), .C(C), .Z(MADD_Z));
+
+        // Output mux
+        assign Z = INST[4] ? MADD_Z : Alu_Z;
+
 endmodule
