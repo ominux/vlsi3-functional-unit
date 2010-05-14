970519ccecf0bc0b38e16867405f1b9e23fd4806
uwjimmyxu <uwjimmyxu@0c9581b5-2034-7111-aa85-6f84924adf1f>
2010-05-14 21:46:35 +0000

    clocked FU

--- trunk/verilog/functional_unit.v
+++ trunk/verilog/functional_unit.v
@@ -7,7 +7,7 @@
 
 `timescale 1ns / 10 ps
 
-module functional_unit(Z, FLAGS, A, B, C, INST, CI);
+module functional_unit(Z, FLAGS, A, B, C, INST, CI, CLOCK);
 	
 	////////
 	// IO //
@@ -17,8 +17,16 @@ module functional_unit(Z, FLAGS, A, B, C, INST, CI);
 	input [31:0] A, B, C;
 	input [4:0] INST;
 	input CI;
+	input CLOCK;
 	
-        wire [31:0] Alu_Z, MADD_Z;
+	wire [31:0] Z1;
+        wire [31:0] Alu_Z, MADD_Z, BS_Z;
+	wire Gated_clock_ALU, Gated_clock_BS, Gated_clock_MADD;
+	reg [31:0] Alu_Z1, MADD_Z1, BS_Z1;
+
+	and alu_gate(Gated_clock_ALU, CLOCK, INST[4]);
+	and bs_gate(Gated_clock_BS, CLOCK, INST[3]);
+	and madd_gate(Gated_clock_MADD, CLOCK, INST[2]);
 
 	// alu
 	Alu alu_inst (.A(A),
@@ -32,12 +40,34 @@ module functional_unit(Z, FLAGS, A, B, C, INST, CI);
                       .FLAGS());
 		
 	// barrel shifter
-	
+	ShiftLR shifter( .Z(BS_Z), .X(A), .S(B[4:0]), .B(B[5]), .LOG(B[6]) );
 
 	// MADD unit
-        MADD madd (.A(A), .B(B), .C(C), .Z(MADD_Z));
+        //MADD madd (.A(A), .B(B), .C(C), .Z(MADD_Z));
+	
+
+
+
+	// CLOCK GATING
+	always @(posedge Gated_clock_ALU)
+	begin
+		Alu_Z1 = Alu_Z;
+	end
+
+
+	always @(posedge Gated_clock_BS)
+	begin
+		BS_Z1 = BS_Z;
+	end
+
+	always @(posedge Gated_clock_MADD)
+	begin
+		MADD_Z1 = MADD_Z;
+	end
+
 
         // Output mux
-        assign Z = INST[4] ? MADD_Z : Alu_Z;
+	assign Z = INST[3] ? (INST[4] ? MADD_Z1 : Alu_Z1) : BS_Z1;
+
 
 endmodule
