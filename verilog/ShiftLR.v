58da4ac7d64c76903f2d7b7c5e71394a60793903
coreybolson <coreybolson@0c9581b5-2034-7111-aa85-6f84924adf1f>
2010-05-24 18:49:19 +0000

    shifter back to being purely combinational

--- verilog/ShiftLR.v
+++ verilog/ShiftLR.v
@@ -2,18 +2,18 @@
 
 File: Bi-directional shifter used in the 
 			functional unit of the Mosaic project.
+			This unit is purely comibinational.
 
 Author: Corey Olson
 
 Date: 5/13/2010
 
 */
-module ShiftLR( Z, X, S, LEFT, LOG , CLOCK);
+module ShiftLR( Z, X, S, LEFT, LOG);
 input [31:0] X;
 input [4:0] S;
 input LEFT;
 input LOG;
-input CLOCK;
 output [31:0] Z;
 
 // intermediate wires needed
@@ -30,23 +30,10 @@ wire [31:0] shift0;
 // _n = inverted signal
 
 // insert these so we can use clock gating on the inputs
-reg [31:0] input_cg;
-reg [4:0] shift_cg;
-wire [4:0] shift_cg_n;
-reg left_cg, log_cg;
+wire [4:0] shift_n;
 
 // used to assign intermediate input
 integer i;
-
-/////////////////////
-// Latch the Input //
-/////////////////////
-always @ (posedge CLOCK) begin
-	input_cg <= X;
-	shift_cg <= S;
-	left_cg <= LEFT;
-	log_cg <= LOG;
-end
 
 ///////////////////////////////
 // set up inputs  to shifter //
@@ -54,19 +41,19 @@ end
 always @ (*) begin
 	// shift left
 	// log & arith - input is {data,31'0}
-	if(left_cg) begin
-		mux_in = {input_cg[30:0],32'b0};
+	if(LEFT) begin
+		mux_in = {X[30:0],32'b0};
 	end
 	// shift right
 	// logical - input is {31'0,data}
-	else if(log_cg) begin
-		mux_in = {31'b0,input_cg};
+	else if(LOG) begin
+		mux_in = {31'b0,X};
 	end
 	// arithmetic - {31' sign extend,data}
 	else begin
-		mux_in[31:0] = input_cg;
+		mux_in[31:0] = X;
 		for (i=32;i<63;i=i+1) begin
-			mux_in[i]=input_cg[31];
+			mux_in[i]=X[31];
 		end
 	end
 end
@@ -76,12 +63,12 @@ end
 // use 2's complement if LEFT is asserted //
 // else assume right shift                //
 ////////////////////////////////////////////
-assign shift_cg_n = ~shift_cg;
-assign shift = left_cg ? {shift_cg_n[4]^(&shift_cg_n[3:0]),
-															shift_cg_n[3]^(&shift_cg_n[2:0]),
-															shift_cg_n[2]^(&shift_cg_n[1:0]),
-															shift_cg_n[1]^shift_cg_n[0],
-															shift_cg[0]} : shift_cg;
+assign shift_n = ~S;
+assign shift = LEFT ?	{shift_n[4]^(&shift_n[3:0]),
+											shift_n[3]^(&shift_n[2:0]),
+											shift_n[2]^(&shift_n[1:0]),
+											shift_n[1]^shift_n[0],
+											S[0]} : S;
 
 /////////////////
 // LOG SHIFTER //
@@ -101,6 +88,8 @@ assign shift0 = shift[0] ? shift1[32:1] : shift1[31:0];
 // NOTE: Shifting the way that we do makes it impossible to shift left by 0 bits, 
 //				so this is a hack to fix that.  we could simply not support a shift left
 //				of 0 bits, and that should be considered
-assign Z = |shift ? shift0[31:0] : input_cg;
+assign Z = |shift ? shift0[31:0] : X;
+
+initial $monitor("Input:%h, output:%h, shift:%h, left:%b, logical:%b",X,Z,S,LEFT,LOG);
 
 endmodule
--- verilog/ShiftLR_tb.v
+++ verilog/ShiftLR_tb.v
@@ -16,7 +16,7 @@ Date: 5/13/2010
 module ShiftLR_tb();
 	
 	// inputs to the shifter
-	reg [31:0] input_data;
+	reg signed [31:0] input_data;
 	reg [4:0] shift_amount;
 	reg left,logical;
 
@@ -25,49 +25,42 @@ module ShiftLR_tb();
 
 	// create a clock to control shifting
 	reg clk;
-	// latch the data for comparison with the output data
-	reg signed [31:0] input_cg;
-	reg [4:0] shift_amount_cg;
-	reg left_cg, log_cg;
 	
 	// instantiate the barrel shifter here
 	ShiftLR inst_shift (.X(input_data),.S(shift_amount),
 											.LEFT(left),.LOG(logical),
-											.CLOCK(clk), .Z(output_data));
+											.Z(output_data));
 
 	// vary the inputs and control the outputs
 	// $dumpvars
 	initial begin
 		$monitor("Input data:%h, output data:%h, shift:%h, left:%b, logical:%b",
-							input_cg,output_data,shift_amount_cg,left_cg,log_cg);
+							input_data,output_data,shift_amount,left,logical);
 		left = 0;
 		clk = 0;
 		logical = 0;
 		shift_amount = 0;
 		
 		$display("Testing Arithmetic Shift Right...");
-		#448 logical = 1;
+		repeat (32) @ (posedge clk);
+		logical <= 1;
 		$display("Testing Logical Shift Right...");
-		#448 left = 1;
+		repeat (32) @ (posedge clk);
+		left <= 1;
 		$display("Testing Logical Shift Left...");
-		#448 logical = 0;
+		repeat (32) @ (posedge clk);
+		logical <= 0;
 		$display("Testing Arithmetic Shift Left...");
-		#448 $display("Shifter Test completed Successfully");
+		repeat (32) @ (posedge clk);
+		$display("Shifter Test completed Successfully");
 		$stop;
 	end
 
-	// create the clock (700MHz ~ 1.4ns)
-	always #7 clk <= ~clk;
+	// create the clock (1GHz = 1ns period)
+	always #5 clk <= ~clk;
 	
 	// change the input data and shift amount to random values
 	always @ (posedge clk) begin
-		// output data will be delayed by 1 clock
-		input_cg <= input_data;
-		shift_amount_cg <= shift_amount;
-		log_cg <= logical;
-		left_cg <= left;
-	
-		// shift random data and a random amount
 		input_data <= $random;
 		shift_amount <= shift_amount + 1'b1;
 	end
@@ -77,29 +70,29 @@ module ShiftLR_tb();
 
 		// check that we are always doing the correct shifting
 		// check all left shifts
-		if (left_cg) begin
-			if (output_data !== input_cg<<shift_amount_cg) begin
+		if (left) begin
+			if (output_data !== input_data<<shift_amount) begin
 				$display("ERROR: Shift Left Incorrect\n");
 				$display("Output data:%h, input data:%h, amount:%h, should be:%h",
-									output_data,input_cg,shift_amount_cg,input_cg<<shift_amount_cg);
+									output_data,input_data,shift_amount,input_data<<shift_amount);
 				$stop;
 			end
 		end
 		// logical shift right
-		else if(log_cg) begin
-			if (output_data !== input_cg>>shift_amount_cg) begin
+		else if(logical) begin
+			if (output_data !== input_data>>shift_amount) begin
 				$display("ERROR: Logical Shift Right Incorrect\n");
 				$display("Output data:%h, input data:%h, amount:%h, should be:%h",
-									output_data,input_cg,shift_amount_cg,input_cg>>shift_amount_cg);
+									output_data,input_data,shift_amount,input_data>>shift_amount);
 				$stop;
 			end
 		end
 		// arithmetic shift right
 		else begin
-			if (output_data !== input_cg>>>shift_amount_cg) begin
+			if (output_data !== input_data>>>shift_amount) begin
 				$display("ERROR: Arithmetic Shift Right Incorrect\n");
 				$display("Output data:%h, input data:%h, amount:%h, should be:%h",
-									output_data,input_cg,shift_amount_cg,input_cg>>>shift_amount_cg);
+									output_data,input_data,shift_amount,input_data>>>shift_amount);
 				$stop;
 			end
 		end
