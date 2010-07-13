d72641ec2f47950ef80b0347677bf6e76d16bfa2
coreybolson <coreybolson@0c9581b5-2034-7111-aa85-6f84924adf1f>
2010-07-13 18:25:18 +0000

    modified ALU to support all needed operations

--- verilog/Alu.v
+++ verilog/Alu.v
@@ -21,36 +21,47 @@ Notes:
 
 */
 `include "adder_struct.v"
-module Alu( Z, A, B, INST, FLAGS);
+module Alu( Z, A, B, INST, SEL);
 	
 	////////
 	// IO //
 	////////
+	output	[31:0]	Z;			// output data
 	input		[31:0]	A;			// First Input Data, subtract is A-B
 	input		[31:0]	B;			// Second Input Data, this is added to or subtracted from A
 	input		[3:0]		INST;		// Instructions are defined in the localparam sectionnnnn
-	output	[31:0]	Z;			// output data
-	output	[3:0]		FLAGS;	// Flags = {reserved,Zero,CarryOut,Overflow}
+	input						SEL;		// Select bit for select operations
 
 	////////////////////////////////////////
 	// Instructions supported by this ALU //
 	////////////////////////////////////////
-	localparam [3:0]	add_1 = 4'b0000,	sub_1 = 4'b0001,	add_ab=4'b0010, 
-										sub_ab = 4'b0011,	abs_a = 4'b0100,	neg_a = 4'b0101, 
-										neg_b =4'b0111,		and_ab = 4'b1000,	or_ab = 4'b1001, 
-										xor_ab = 4'b1010,	not_b =4'b1011,		i_a = 4'b1100,
-										not_a = 4'b1101,	i_0 = 4'b1110,		i_1 = 4'b1111;
+	localparam [3:0]	add_ab		= 4'b0000,	
+										neg_a			= 4'b0001,	
+										and_ab		= 4'b0010, 
+										or_ab			= 4'b0011,
+										xor_ab		= 4'b0100,	
+										inv_a			= 4'b0101, 
+										sel_ab		= 4'b0110, 
+										sel_ba		= 4'b0111, 
+										sub_ab		= 4'b1000,	
+										a_lt_b		= 4'b1001, 
+										a_lte_b		= 4'b1010,	
+										a_gt_b		= 4'b1011, 
+										a_gte_b		= 4'b1100,
+										a_eq_b		= 4'b1101,	
+										a_ne_b		= 4'b1110, 
+										sel_xor_b	= 4'b1111;
 	
-	//////////////////
-	// Clock Gating //
-	//////////////////
+	////////////
+	// Output //
+	////////////
 	reg [31:0] Z;
 	
 	////////////////////////
 	// Intermediate Nodes //
 	////////////////////////
 	wire [31:0] adder_out, P, G, A_or_B;
-	wire adder_co, overflow;
+	wire adder_co, overflow, zero;
 
 	///////////////////////////
 	// Modified Adder Inputs //
@@ -64,29 +75,8 @@ module Alu( Z, A, B, INST, FLAGS);
 		cin = 1'b0;
 		
 		case (INST)
-			// Z = A + 1
-			add_1: begin
-				bin = 32'b0;
-				cin = 1'b1;
-			end
-			// Z = A - 1
-			sub_1: begin
-				bin = 32'hFFFFFFFF;
-			end
 			// Z = A + B
 			add_ab:	begin
-			end
-			// Z = A - B
-			sub_ab: begin
-				bin = ~B;
-				cin = 1'b1;
-			end
-			// Z = abs(A)
-			abs_a: begin
-				if (A[31]) ain = ~A;
-				else ain = A;
-				bin = 32'b0;
-				cin = A[31];
 			end
 			// Z = -1 * A
 			neg_a: begin
@@ -94,44 +84,71 @@ module Alu( Z, A, B, INST, FLAGS);
 				bin = 32'b0;
 				cin = 1'b1;
 			end
-			// Z = -1 * B
-			neg_b: begin
-				ain = 32'b0;
-				bin = ~B;
-				cin = 1'b1;
-			end
 			// Z = A & B
 			and_ab: begin
 			end
+			// Z = A | B
+			or_ab: begin
+			end
 			// Z = A ^ B
-			xor_ab: begin
-			end
-			// Z = ~B
-			not_b: begin
-				ain = 32'b0;
-				bin = ~B;
-			end
-			// Z = A
-			i_a: begin
-				bin = 32'b0;
+			xor_ab:	begin
 			end
 			// Z = ~A
-			not_a: begin
+			inv_a: begin
 				ain = ~A;
 				bin = 32'b0;
 			end
-			// Z = 32'h0
-			i_0: begin
-				ain = 32'b0;
-				bin = 32'b0;
+			// Z = (~SEL & A) | (SEL & B)
+			sel_ab: begin
+				if (SEL) ain = 32'b0;
+				else bin = 32'b0;
 			end
-			// Z = 32'hFFFFFFFF
-			i_1: begin
-				ain = 32'hFFFFFFFF;
-				bin = 32'b0;
+			// Z = (SEL & A) | (~SEL & B)
+			sel_ba: begin
+				if (SEL) bin = 32'b0;
+				else ain = 32'b0;
+			end
+			// Z = A - B
+			sub_ab:	begin
+				bin = ~B;
+				cin = 1'b1;
+			end
+			// Z = {31'b0,A<B}
+			a_lt_b: begin
+				bin = ~B;
+				cin = 1'b1;
+			end
+			// Z = {31'b0,A<=B}
+			a_lte_b: begin
+				bin = ~B;
+				cin = 1'b1;
+			end
+			// Z = {31'b0,A>B}
+			a_gt_b: begin
+				bin = ~B;
+				cin = 1'b1;
+			end
+			// Z = {31'b0,A>=B}
+			a_gte_b: begin
+				bin = ~B;
+				cin = 1'b1;
+			end
+			// Z = {31'b0,A==B}
+			a_eq_b: begin
+				bin = ~B;
+				cin = 1'b1;
+			end
+			// Z = {31'b0,A!=B}
+			a_ne_b: begin
+				bin = ~B;
+				cin = 1'b1;
+			end
+			// Z = {31'b0,SEL^B}
+			sel_xor_b: begin
+				ain = {31'b0,SEL};
 			end
 			// let the synthesis tool optimize the other cases
-			default begin
+			default: begin
 			end
 		endcase
 	end
@@ -151,6 +168,13 @@ module Alu( Z, A, B, INST, FLAGS);
 			and_ab: Z = G;
 			or_ab: Z = A_or_B;
 			xor_ab: Z = P;
+			a_lt_b: Z = {31'b0,adder_out[31]^overflow};
+			a_lte_b: Z = {31'b0,(adder_out[31]^overflow)|zero};
+			a_gt_b: Z = {31'b0,~adder_out[31]^overflow};
+			a_gte_b: Z = {31'b0,(~adder_out[31]^overflow)|zero};
+			a_eq_b: Z = {31'b0,zero};
+			a_ne_b: Z = {31'b0,~zero};
+			sel_xor_b: Z = P;
 			default: Z = adder_out;
 		endcase
 	end
@@ -158,9 +182,6 @@ module Alu( Z, A, B, INST, FLAGS);
 	///////////
 	// Flags //
 	///////////
-	assign FLAGS[3] = 1'b0;
-	assign FLAGS[2] = ~(|Z);			//(Z == 32'b0);
-	assign FLAGS[1] = adder_co;
-	assign FLAGS[0] = overflow;
+	assign zero = ~(|adder_out);
 
 endmodule
