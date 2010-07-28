ebd98e9d6c4493bf5c228c881dded0cf6d719f07
felispuma@gmail.com <felispuma@gmail.com@0c9581b5-2034-7111-aa85-6f84924adf1f>
2010-07-28 08:45:33 +0000

    Added latched versions with enables

---
+++ trunk/verilog/ShiftLR_latch.v
@@ -0,0 +1,103 @@
+/*
+
+File: Bi-directional shifter used in the 
+			functional unit of the Mosaic project.
+			This unit is purely comibinational.
+
+Author: Corey Olson
+
+Date: 5/13/2010
+
+*/
+module ShiftLR( Z, X, S, LEFT, LOG, EN);
+input [31:0] X;
+input [4:0] S;
+input LEFT;
+input LOG;
+output [31:0] Z;
+input EN;
+
+// intermediate wires needed
+wire [4:0] shift;
+reg [62:0] mux_in;
+wire [46:0] shift4;
+wire [38:0] shift3;
+wire [34:0] shift2;
+wire [32:0] shift1;
+wire [31:0] shift0;
+reg [31:0] X_latch;
+
+// Naming conventions:
+// _cg = clock gated
+// _n = inverted signal
+
+// insert these so we can use clock gating on the inputs
+wire [4:0] shift_n;
+
+// used to assign intermediate input
+integer i;
+
+///////////////////////////////
+// set up inputs  to shifter //
+///////////////////////////////
+
+// Latch input
+always @*
+  if (EN)
+    X_latch <= X;
+
+always @ (*) begin
+	// shift left
+	// log & arith - input is {data,31'0}
+	if(LEFT) begin
+		mux_in = {X_latch[30:0],32'b0};
+	end
+	// shift right
+	// logical - input is {31'0,data}
+	else if(LOG) begin
+		mux_in = {31'b0,X_latch};
+	end
+	// arithmetic - {31' sign extend,data}
+	else begin
+		mux_in[31:0] = X_latch;
+		for (i=32;i<63;i=i+1) begin
+			mux_in[i]=X_latch[31];
+		end
+	end
+end
+
+////////////////////////////////////////////
+// set up select bits                     //
+// use 2's complement if LEFT is asserted //
+// else assume right shift                //
+////////////////////////////////////////////
+assign shift_n = ~S;
+assign shift = LEFT ?	{shift_n[4]^(&shift_n[3:0]),
+											shift_n[3]^(&shift_n[2:0]),
+											shift_n[2]^(&shift_n[1:0]),
+											shift_n[1]^shift_n[0],
+											S[0]} : S;
+
+/////////////////
+// LOG SHIFTER //
+/////////////////
+// 0th stage, shift by 16
+assign shift4 = shift[4] ? mux_in[62:16] : mux_in[46:0];
+// 1st stage, shift by 8
+assign shift3 = shift[3] ? shift4[46:8] : shift4[38:0];
+// 2nd stage, shift by 4
+assign shift2 = shift[2] ? shift3[38:4] : shift3[34:0];
+// 3rd stage, shift by 2
+assign shift1 = shift[1] ? shift2[34:2] : shift2[32:0];
+// 4th stage, shift by 1
+assign shift0 = shift[0] ? shift1[32:1] : shift1[31:0];
+
+// final output
+// NOTE: Shifting the way that we do makes it impossible to shift left by 0 bits, 
+//				so this is a hack to fix that.  we could simply not support a shift left
+//				of 0 bits, and that should be considered
+assign Z = |shift ? shift0[31:0] : X_latch;
+
+initial $monitor("Input:%h, output:%h, shift:%h, left:%b, logical:%b",X,Z,S,LEFT,LOG);
+
+endmodule
---
+++ trunk/verilog/ShiftLR_tb_latch.v
@@ -0,0 +1,108 @@
+`timescale 100ps / 10ps
+/*
+
+File: Testbench for the bi-directional shifter used 
+			in the functional unit of the Mosaic project.
+
+Author: Corey Olson
+
+Date: 5/13/2010
+
+*/
+
+`include "ShiftLR.v"
+//`include "ShiftLR.vh"
+
+module ShiftLR_tb();
+	
+	// inputs to the shifter
+	reg signed [31:0] input_data, input_data_latch;
+	reg [4:0] shift_amount;
+	reg left,logical,en;
+
+	// outputs from the shifter
+	wire signed [31:0] output_data;
+
+	// create a clock to control shifting
+	reg clk;
+	
+	// instantiate the barrel shifter here
+	ShiftLR inst_shift (.X(input_data),.S(shift_amount),
+											.LEFT(left),.LOG(logical), .EN(en),
+											.Z(output_data));
+
+	// vary the inputs and control the outputs
+	// $dumpvars
+	initial begin
+		$monitor("Input data:%h, output data:%h, shift:%h, left:%b, logical:%b",
+							input_data,output_data,shift_amount,left,logical);
+		left = 0;
+		clk = 0;
+		logical = 0;
+		shift_amount = 0;
+                en = 1;
+		
+		$display("Testing Arithmetic Shift Right...");
+		repeat (32) @ (posedge clk);
+		logical <= 1;
+		$display("Testing Logical Shift Right...");
+		repeat (32) @ (posedge clk);
+		left <= 1;
+		$display("Testing Logical Shift Left...");
+		repeat (32) @ (posedge clk);
+		logical <= 0;
+		$display("Testing Arithmetic Shift Left...");
+		repeat (32) @ (posedge clk);
+		$display("Shifter Test completed Successfully");
+		$stop;
+	end
+
+	// create the clock (1GHz = 1ns period)
+	always #5 clk <= ~clk;
+	
+	// change the input data and shift amount to random values
+	always @ (posedge clk) begin
+		input_data <= $random;
+                en <= $random;
+		shift_amount <= shift_amount + 1'b1;
+	end
+
+        always @*
+          if (en)
+            input_data_latch <= input_data;
+
+	// check that the shifted data is correct
+	always @ (posedge clk) begin
+
+		// check that we are always doing the correct shifting
+		// check all left shifts
+		if (left) begin
+			if (output_data !== input_data_latch<<shift_amount) begin
+				$display("ERROR: Shift Left Incorrect\n");
+				$display("Output data:%h, input data:%h, amount:%h, should be:%h",
+									output_data,input_data,shift_amount,input_data<<shift_amount);
+				$stop;
+			end
+		end
+		// logical shift right
+		else if(logical) begin
+			if (output_data !== input_data_latch>>shift_amount) begin
+				$display("ERROR: Logical Shift Right Incorrect\n");
+				$display("Output data:%h, input data:%h, amount:%h, should be:%h",
+									output_data,input_data,shift_amount,input_data>>shift_amount);
+				$stop;
+			end
+		end
+		// arithmetic shift right
+		else begin
+			if (output_data !== input_data_latch>>>shift_amount) begin
+				$display("ERROR: Arithmetic Shift Right Incorrect\n");
+				$display("Output data:%h, input data:%h, amount:%h, should be:%h",
+									output_data,input_data,shift_amount,input_data>>>shift_amount);
+				$stop;
+			end
+		end
+		
+	end
+
+endmodule
